870cf4f520919262bbc3f379ffa1bf684a84446c
Alberto Cuesta Cañada <mail@albertocuesta.es>
2021-01-14 13:30:23 +0000

    feat: transferFromWithSignature drafted

--- contracts/ERC20Permit.sol
+++ contracts/ERC20Permit.sol
@@ -3,21 +3,18 @@
 pragma solidity ^0.6.0;
 
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
-import "./IERC2612.sol";
 
 /**
- * @author Georgios Konstantopoulos
- * @dev Extension of {ERC20} that allows token holders to use their tokens
- * without sending any transactions by setting {IERC20-allowance} with a
- * signature using the {permit} method, and then spend them via
- * {IERC20-transferFrom}.
+ * @author Alberto Cuesta Cañada
+ * @dev Extension of {ERC20} that allows token holders set allowances or approve a single `transferFrom` using off-chain signatures.
  *
- * The {permit} signature mechanism conforms to the {IERC2612} interface.
+ * The mechanisms don't conform to the {IERC2612} interface.
  */
-abstract contract ERC20Permit is ERC20, IERC2612 {
-    mapping (address => uint256) public override nonces;
+abstract contract ERC20Permit is ERC20 {
+    mapping (address => uint256) public nonces;
 
     bytes32 public immutable PERMIT_TYPEHASH = keccak256("Permit(address owner,address spender,uint256 value,uint256 nonce,uint256 deadline)");
+    bytes32 public immutable TRANSFERFROM_TYPEHASH = keccak256("TransferFrom(address sender,address recipient,uint256 value,uint256 nonce,uint256 deadline)");
     bytes32 public immutable DOMAIN_SEPARATOR;
 
     constructor(string memory name_, string memory symbol_) internal ERC20(name_, symbol_) {
@@ -38,12 +35,18 @@ abstract contract ERC20Permit is ERC20, IERC2612 {
     }
 
     /**
-     * @dev See {IERC2612-permit}.
+     * @dev Similar to {IERC2612-permit}, but with a packed signature.
      *
      * In cases where the free option is not a concern, deadline can simply be
-     * set to uint(-1), so it should be seen as an optional parameter
+     * set to uint(-1), so it should be seen as an optional parameter.
      */
-    function permit(address owner, address spender, uint256 amount, uint256 deadline, uint8 v, bytes32 r, bytes32 s) public virtual override {
+    function permit(
+        address owner,
+        address spender,
+        uint256 amount,
+        uint256 deadline,
+        bytes memory signature
+    ) public virtual {
         require(deadline >= block.timestamp, "ERC20Permit: expired deadline");
 
         bytes32 hashStruct = keccak256(
@@ -65,12 +68,68 @@ abstract contract ERC20Permit is ERC20, IERC2612 {
             )
         );
 
+        (bytes32 r, bytes32 s, uint8 v) = unpack(signature);
         address signer = ecrecover(hash, v, r, s);
         require(
             signer != address(0) && signer == owner,
-            "ERC20Permit: invalid signature"
+            "ERC20Permit::permit: invalid signature"
         );
 
         _approve(owner, spender, amount);
+    }
+
+    /**
+     * @dev Similar to {IERC20-transferFrom}, but with a packed signature. It doesn't check or change allowances.
+     *
+     * In cases where the free option is not a concern, deadline can simply be
+     * set to uint(-1), so it should be seen as an optional parameter.
+     */
+    function transferFromWithSignature(
+        address sender,
+        address recipient,
+        uint256 amount,
+        uint256 deadline,
+        bytes memory signature
+    ) public virtual returns(bool) {
+        require(deadline >= block.timestamp, "ERC20Permit: expired deadline");
+
+        bytes32 hashStruct = keccak256(
+            abi.encode(
+                TRANSFERFROM_TYPEHASH,
+                sender,
+                recipient,
+                amount,
+                nonces[sender]++,
+                deadline
+            )
+        );
+
+        bytes32 hash = keccak256(
+            abi.encodePacked(
+                '\x19\x01',
+                DOMAIN_SEPARATOR,
+                hashStruct
+            )
+        );
+
+        (bytes32 r, bytes32 s, uint8 v) = unpack(signature);
+        address signer = ecrecover(hash, v, r, s);
+        require(
+            signer != address(0) && signer == sender,
+            "ERC20Permit::transferFrom: invalid signature"
+        );
+
+        _transfer(sender, recipient, amount);
+        return true;
+    }
+
+    /// @dev Unpack r, s and v from a `bytes` signature.
+    /// @param signature A packed signature.
+    function unpack(bytes memory signature) internal pure returns (bytes32 r, bytes32 s, uint8 v) {
+        assembly {
+            r := mload(add(signature, 0x20))
+            s := mload(add(signature, 0x40))
+            v := byte(0, mload(add(signature, 0x60)))
+        }
     }
 }
